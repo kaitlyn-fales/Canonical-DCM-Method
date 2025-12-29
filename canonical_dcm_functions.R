2ccf393e969878c48144b2a1124e35b31153a189
kaitlyn-fales <krf5429@psu.edu>
2025-12-29 13:48:17 -0500

    source functions for canonical DCM

---
+++ canonical_dcm_functions.R
@@ -0,0 +1,162 @@
+# Function to get changepoints in u when u(t) is any dimension
+get_changepoints <- function(input){
+  
+  # Get dataframe of indices when changes occur
+  changes <- as.data.frame(which(abs(diff(input)) >= 1, arr.ind = T))
+  changes$row <- changes$row+1 # add 1 to row index to indicate the row the change starts
+  changes <- changes[order(changes$row),] # ascending order
+  
+  # Make duplicates their own category to reflect when switching from input1 -> input2
+  changes <- changes[!duplicated(changes$row),] # remove duplicates
+  
+  # Add in last row to reflect final block of times
+  changes <- rbind(changes, c(nrow(input),0))$row
+  
+  # Return dataframe
+  return(changes)
+}
+
+# Function to extract draws from pathfinder for init
+get_pathfinder_init <- function(draws) {
+  last_draws <- draws %>%
+    group_by(.chain) %>%
+    slice_tail(n = 1) %>%
+    ungroup() %>% 
+    select(-starts_with("."))
+  
+  # Extracting prefixes
+  col_names <- names(last_draws)[-c(1:2)]
+  prefixes <- unique(sub("\\[.*", "", col_names))
+  
+  # Use split() to group the column names by their extracted prefix
+  grouped_list <- list()
+  
+  # Extract columns
+  for (prefix in prefixes) {
+    # Find columns matching the current prefix
+    cols_to_extract <- grep(paste0("^", prefix, "\\["), colnames(last_draws), value = TRUE)
+    
+    # Extract these columns and convert to a list
+    sublist_df <- last_draws[, cols_to_extract]
+    grouped_list[[prefix]] <- as.numeric(sublist_df)
+  }
+  
+  return(grouped_list)
+}
+
+# Function to extract last draw for next init to keep chain going
+get_last_draws_for_init <- function(draws) {
+  last_draws <- draws %>%
+    group_by(.chain) %>%
+    slice_tail(n = 1) %>%
+    ungroup() %>% 
+    select(-starts_with("."))
+  
+  # Extracting prefixes
+  col_names <- names(last_draws)[-1]
+  prefixes <- unique(sub("\\[.*", "", col_names))
+  
+  # Use split() to group the column names by their extracted prefix
+  grouped_list <- list()
+  
+  # Extract columns
+  for (prefix in prefixes) {
+    # Find columns matching the current prefix
+    cols_to_extract <- grep(paste0("^", prefix, "\\["), colnames(last_draws), value = TRUE)
+    
+    # Extract these columns and convert to a list
+    sublist_df <- last_draws[, cols_to_extract]
+    grouped_list[[prefix]] <- as.numeric(sublist_df)
+  }
+  
+  return(grouped_list)
+}
+
+# Function to structure list data into Stan input data for model
+get_stan_dat <- function(data, hypothesis_idxs, ode_solver_type = 1, tol = 10^{-5},
+                         sigma_nu = 1, sigma_nu_self = 0.125, rate_sigma = 0.5, 
+                         rate_z0 = 2, conv = 1, max_num_steps = 10^6){
+  # Assumes input data object is list with following structure:
+  #   times: T x 1 vector with the time index, separated by TR (seconds)
+  #   u: T x n_u matrix encoding the timing of experimental stimuli
+  #   y_obs: T x m matrix encoding the scaled BOLD signal for each ROI
+  #   scale: real number for scale factor of BOLD signal
+  
+  # Assumes input hypothesis_idxs is list with following structure:
+  #   A_idxs: 2-column matrix where each row contains the i,j coord for A matrix hypothesis
+  #   B_idxs: 3-column matrix where each row contains the k,i,j coord for B matrix hypothesis (k is stimulus)
+  #   C_idxs: 2-column matrix where each row contains the i,j coord for C matrix hypothesis
+  
+  # Obtain dataframe of changes based on input
+  input <- rbind(0,data$u)
+  changes <- get_changepoints(input)
+  
+  # Getting data in proper list structure for stan program
+  stan_dat = with(data,
+                  list(T = length(times),
+                       m = ncol(y_obs),
+                       n_u = ncol(u),
+                       n_changes = length(changes),
+                       change_pts = changes,
+                       d_A = nrow(hypothesis_idxs$A_idxs),
+                       d_B = nrow(hypothesis_idxs$B_idxs),
+                       d_C = nrow(hypothesis_idxs$C_idxs),
+                       sigma_nu = sigma_nu,
+                       sigma_nu_self = sigma_nu_self,
+                       rate_sigma = rate_sigma,
+                       rate_z0 = rate_z0,
+                       A_idxs = hypothesis_idxs$A_idxs,
+                       B_idxs = hypothesis_idxs$B_idxs,
+                       C_idxs = hypothesis_idxs$C_idxs,
+                       tp= times,
+                       u = u,
+                       conv = conv, 
+                       y_obs = y_obs,
+                       rel_tol = tol,
+                       abs_tol = tol,
+                       max_num_steps = max_num_steps,
+                       ode_solver_type = ode_solver_type))
+  
+  return(stan_dat)
+}
+
+# Function for using variational Pathfinder to get initial values list, or specifying init = 0 for error
+get_initial_vals <- function(mod, data, pathfinder_init = NULL, num_paths = 1){
+  
+  if (is.null(pathfinder_init) == T) {pathfinder_init <- list(sigma = rep(1,m),
+                                                              nu_A = rep(0,d_A),
+                                                              nu_B = rep(0,d_B),
+                                                              nu_C = rep(0,d_C),
+                                                              z0 = rep(0.1,m))}
+  
+  # tryCatch loop for initial value specification
+  inits_list <- tryCatch(
+    { # Try pathfinder for initial values
+      pathfinder <- mod$pathfinder(data = stan_dat, 
+                                         seed = 1234, 
+                                         init = list(pathfinder_init), 
+                                         num_paths = num_paths)
+      draws <- posterior::as_draws_df(pathfinder$draws())
+      result <- get_pathfinder_init(draws)
+      result
+    },
+    error = function(e) {
+      # Initialize with 0 except for sigma and z0 if pathfinder fails for any reason
+      message("Pathfinder failed: ", e$message)
+      
+      result_backup <- list(list(sigma = rep(1,m),
+                                 nu_A = rep(0,d_A),
+                                 nu_B = rep(0,d_B),
+                                 nu_C = rep(0,d_C),
+                                 z0 = rep(0.1,m)))
+      result_backup
+    }
+  )
+  
+  return(inits_list)
+}
+
+
+
+
+
