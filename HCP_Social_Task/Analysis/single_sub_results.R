e77405377b82888f0d1c0960e68898ec8fb097a1
kaitlyn-fales <krf5429@psu.edu>
2026-01-16 13:00:37 -0500

    update to sampling a subset of subjects for further investigation

--- HCP_Social_Task/Analysis/single_sub_results.R
+++ HCP_Social_Task/Analysis/single_sub_results.R
@@ -1,10 +1,41 @@
 # Examine single subject differences in output parameters
+setwd("/storage/work/krf5429/Canonical-DCM-Method/HCP_Social_Task/Analysis")
 
 library(tidyverse)
 library(R.matlab)
+library(RColorBrewer)
+library(Cairo)
 
-# Choose subject
-sub <- 101107
+# Load diagnostics df after running diagnostics_EDA.R
+load("diagnostics_compilation_final.RData")
+
+# Summary by subject, then sample proportionally from the subjects by group
+subject_summary <- diagnostics_df %>%
+  group_by(subject) %>%
+  summarise(prop_converged = mean(converged)) %>%
+  arrange(desc(prop_converged))
+
+set.seed(1234)
+n_total <- 5
+
+# Compute how many samples per group
+group_counts <- subject_summary %>%
+  count(prop_converged, name = "group_size") %>%
+  mutate(
+    n_to_sample = pmax(1, round(n_total * group_size / sum(group_size)))  # ensure at least 1
+  )
+
+# Apply group-wise sampling
+sampled_subjects <- subject_summary %>%
+  inner_join(group_counts, by = "prop_converged") %>%
+  group_by(prop_converged) %>%
+  group_modify(~ dplyr::slice_sample(.x, n = .x$n_to_sample[1])) %>%
+  ungroup()
+
+sampled_subjects
+
+# Choose subject - run all code below for each subject in sampled_subjects dataframe
+sub <- sampled_subjects$subject[1]
 
 # Phase-encoding (run/session)
 phase <- c("LR","RL")
@@ -17,7 +48,7 @@ combos <- expand.grid(phase = phase,mask = mask_type)
 
 summaries <- list()
 for (j in 1:nrow(combos)){
-  load(paste0("Output/sub-",sub,"_phase",combos$phase[j],"_mask",combos$mask[j],"_draws.RData"))
+  load(paste0("../Output/sub-",sub,"_phase",combos$phase[j],"_mask",combos$mask[j],"_draws.RData"))
   summary <- posterior::summarise_draws(draws_df)[4:15,c(1:2,6:7)]
   summary <- summary %>%
     mutate(across(where(is.numeric), ~round(., digits = 3)))
@@ -37,7 +68,7 @@ df <- df %>%
 df$param_index <- factor(df$param_index, levels = sort(unique(df$param_index)))
 df$phase_mask <- factor(df$phase_mask, levels = c("LR_L", "LR_R", "RL_L", "RL_R"))
 
-# Now plot for one param_group (example: nu_A)
+# Now plot for all param_group 
 ggplot(df,
        aes(x = param_index, y = mean, color = phase_mask)) +
   geom_hline(yintercept = 0, linetype = "dotted", color = "gray40") +
@@ -48,11 +79,14 @@ ggplot(df,
                 position = position_dodge(width = 0.8), width = 0.8) +
   facet_wrap(~param_group, scales = "free_y", labeller = label_parsed) +
   labs(x = "Parameter index", y = "Estimate", color = "Phase mask") +
+  ggtitle(paste0("Subject ",sub)) +
   theme_minimal(base_size = 14) +
   theme(
     panel.grid.major.x = element_blank(),
     panel.grid.minor.x = element_blank()
   )
+dims <- dev.size("in") 
+ggsave(paste0("sub_",sub,"_post_means.png"))
 
 ########### Generate predicted signal from posterior means ###############
 
@@ -119,9 +153,13 @@ input_u = function(t){
 
 ##########################################################################
 
+CairoPNG(paste0("sub_", sub, "_est_vs_obs.png"),
+         width = dims[1], height = dims[2], units = "in", dpi = 96)
+par(mfrow = c(2,2), mar = c(3, 4, 4, 1), oma = c(1, 1, 2, 1))
+
 # Loop through combos, plot est vs obs and export to .mat file for SPM
 for (j in 1:nrow(combos)){
-  load(paste0("Data/sub-",sub,"_phase",combos$phase[j],"_mask",combos$mask[j],".RData"))
+  load(paste0("../Data/sub-",sub,"_phase",combos$phase[j],"_mask",combos$mask[j],".RData"))
   u <- rbind(0,dat$u)
   times <- c(0,dat$times)
   y_obs <- dat$y_obs
@@ -152,7 +190,6 @@ for (j in 1:nrow(combos)){
                atol = 1e-6, 
                rtol = 1e-6
   )
-  diagnostics(out_z)
   
   # Get rid of first column of z (same as times)
   out_z <- out_z[,-1]
@@ -169,24 +206,33 @@ for (j in 1:nrow(combos)){
   y_pred = sapply(1:m, function(i) HRF_mu(out_z[-1,i],times[-1]))
   ##########################################################################
   
-  # Plot estimated vs observed signal
-  titles <- c("V5","p-STS")
-  xlabs <- c("","Time (Seconds)")
+  # Choose colors for each region
+  cols_all <- brewer.pal(8, "Dark2")
+  cols <- cols_all[c(1, 3)]
   
-  par(mfrow = c(2,1), mar = c(2,4,4.5,1.5))
-  for (i in 1:ncol(y_pred)){
-    plot(times[-1],y_obs[,i],type = 'l',xlab = xlabs[i],
-         ylab = "BOLD Response", 
-         ylim = c(min(c(y_obs[,i],y_pred[,i])),
-                  max(c(y_obs[,i],y_pred[,i]))), col = "black",
-         main = titles[i], lty = "dotted")
-    lines(times[-1],y_pred[,i],type = 'l',col = "blue")
+  # ylim range
+  y_range <- range(c(y_pred, y_obs))
+  ylim_exp <- c(y_range[1], y_range[2] * 1.15)
+  
+  # Set up plot
+  plot(times[-1], y_obs[,1], type = "n",  # "n" means no points/lines yet
+       xlab = "Time (s)", ylab = "BOLD Response",
+       ylim = ylim_exp,
+       main = paste0("Phase ", combos$phase[j],
+                     ", Mask ", combos$mask[j]))
+  
+  # Add observed (dashed) and predicted (solid) lines by region
+  for (i in 1:ncol(y_pred)) {
+    lines(times[-1], y_obs[,i], col = cols[i], lty = 3, lwd = 1.5)   # dashed
+    lines(times[-1], y_pred[,i], col = cols[i], lty = 1, lwd = 1.5)  # solid
   }
-  mtext(paste0("Subject ",sub,", Phase ",combos$phase[j],", Mask ",combos$mask[j]), 
-        line = -1.5, outer = T)
+  
+  # Add a legend
+  legend("top", legend = c("V5","p-STS"), horiz = T,
+         col = cols, lty = 1, lwd = 2, bty = "n")
   
   # Save estimated signal to compare with SPM later
-  save(y_pred, paste0("sub_",sub,"_phase",combos$phase[j],"_mask",combos$mask[j],"pred.RData"))
+  save(y_pred, file = paste0("sub_",sub,"_phase",combos$phase[j],"_mask",combos$mask[j],"_pred.RData"))
   
   # Write observed signal to a MATLAB file for SPM
   writeMat(paste0("sub_",sub,"_phase",combos$phase[j],"_mask",combos$mask[j],".mat"),
@@ -194,3 +240,7 @@ for (j in 1:nrow(combos)){
            Y = dat$y_obs)
 }
 
+# Add Subject title to plot
+mtext(paste0("Predicted (Solid) vs. Observed (Dotted): Subject ",sub), line = 0, outer = T)
+
+dev.off()
