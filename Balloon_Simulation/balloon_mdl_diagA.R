68c9fe87b4da6d5a9c80e6ed3b7b6e3dd3808b35
kaitlyn-fales <krf5429@psu.edu>
2026-01-20 14:12:54 -0500

    update ess check function

--- Balloon_Simulation/balloon_mdl_diagA.R
+++ Balloon_Simulation/balloon_mdl_diagA.R
@@ -20,9 +20,6 @@ load("balloon_sim_data_diagA.RData")
 output_dir <- "Output"
 basename <- paste0("balloon_mdl_diagA",index)
 
-# Convergence check specs - 90% intervals with 5% tolerance
-ess_check <- as.numeric(minESS(10, alpha = 0.1, eps = 0.05))
-
 # Set up hypotheses
 idxs = list(A_idxs = matrix(c(2,1,
                               1,2,
@@ -36,6 +33,10 @@ source("../canonical_dcm_functions.R")
 
 # Put data into form for sampler
 stan_dat <- get_stan_dat(dat, idxs)
+
+# Convergence check specs - 90% intervals with 5% tolerance
+num_param <- get_num_param(stan_dat)
+ess_check <- as.numeric(minESS(num_param, alpha = 0.1, eps = 0.05))
 
 # Compile stan program from parent directory
 canonical_dcm = cmdstanr::cmdstan_model("../canonical_dcm.stan")
--- Balloon_Simulation/balloon_mdl_diagAB.R
+++ Balloon_Simulation/balloon_mdl_diagAB.R
@@ -20,9 +20,6 @@ load("balloon_sim_data_diagAB.RData")
 output_dir <- "Output"
 basename <- paste0("balloon_diagAB",index)
 
-# Convergence check specs - 90% intervals with 5% tolerance
-ess_check <- as.numeric(minESS(11, alpha = 0.1, eps = 0.05))
-
 # Set up hypotheses
 idxs = list(A_idxs = matrix(c(2,1,
                               1,2,
@@ -37,6 +34,10 @@ source("../canonical_dcm_functions.R")
 
 # Put data into form for sampler
 stan_dat <- get_stan_dat(dat, idxs)
+
+# Convergence check specs - 90% intervals with 5% tolerance
+num_param <- get_num_param(stan_dat)
+ess_check <- as.numeric(minESS(num_param, alpha = 0.1, eps = 0.05))
 
 # Compile stan program from parent directory
 canonical_dcm = cmdstanr::cmdstan_model("../canonical_dcm.stan")
