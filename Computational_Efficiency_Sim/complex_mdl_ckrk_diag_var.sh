f2bef305dcd57b9e241712d201976836caa55aae
kaitlyn-fales <krf5429@psu.edu>
2026-01-20 14:47:12 -0500

    update slurm job

--- Computational_Efficiency_Sim/complex_mdl_ckrk_diag_var.sh
+++ Computational_Efficiency_Sim/complex_mdl_ckrk_diag_var.sh
@@ -4,8 +4,7 @@
 #SBATCH --ntasks=1 
 #SBATCH --mem-per-cpu=4gb
 #SBATCH --time=48:00:00
-#SBATCH --account=open
-#SBATCH --partition=open
+#SBATCH --account=statsresearch_sc_default
 #SBATCH --output=Output/complex_mdl_ckrk_diag_var_%a.out
 #SBATCH --array=1-5
 
