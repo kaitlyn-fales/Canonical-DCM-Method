b4760516fe8dae0b64bd934d216009c2f019fb84
kaitlyn-fales <krf5429@psu.edu>
2026-02-11 11:15:27 -0500

    adding between-subject uncertainty plot

--- HCP_Social_Task/group_results.R
+++ HCP_Social_Task/group_results.R
@@ -72,6 +72,67 @@ grid.arrange(p1, p2, p3, p4, ncol=2, nrow=2,
 
 ###################################################################
 
+################ Comparing between-subject SD and HPD intervals ##################
+
+plot_titles <- c("Phase LR Encoding and Mask L",
+                 "Phase LR Encoding and Mask R",
+                 "Phase RL Encoding and Mask L",
+                 "Phase RL Encoding and Mask R")
+
+# For loop to make plots
+for (i in 1:length(MCMC_results)){
+  MCMC <- MCMC_results[[i]]
+  SPM <- SPM_results[[i]]
+  
+  MCMC$Method <- "Canonical DCM"
+  SPM$Method <- "SPM"
+  combined <- rbind(MCMC, SPM)
+  
+  p <- ggplot(combined, aes(x = factor(parameter), y = between_subject_sd, group = Method, colour = Method)) +
+    geom_point(aes(shape = Method), size = 2.5, position = position_dodge(width = 0.6)) +
+    geom_errorbar(aes(ymin = between_subject_q2.5, ymax = between_subject_q97.5, group = Method),
+                  width = 0.6, position = position_dodge(width = 0.6)) +
+    geom_hline(yintercept = 0, linetype = "dotted") +
+    geom_vline(xintercept = c(4.5, 8.5), linetype = "dashed") +
+    ggtitle(plot_titles[i]) +
+    labs(x = "", y = "") + ylim(c(-0.15,1.4)) +
+    scale_color_brewer(palette = "Dark2") +
+    annotate("text", x = 2.5, y = -0.1, label = "hat(tau)[A]", parse = TRUE, size = 7) +
+    annotate("text", x = 6.5, y = -0.1, label = "hat(tau)[B]", parse = TRUE, size = 7) +
+    annotate("text", x = 9.5, y = -0.1, label = "hat(tau)[C]", parse = TRUE, size = 7) +
+    theme(
+      axis.text.x = element_blank(),
+      axis.ticks.x = element_blank(),
+      panel.background = element_blank(),
+      axis.line = element_line(color = "black"),
+      axis.title = element_text(size = 12, face = "bold"),
+      axis.text.y = element_text(size = 12, face = "bold"),
+      plot.title = element_text(size = 16),
+      legend.position = c(0, 1),       
+      legend.justification = c(0, 1), 
+      legend.text = element_text(size = 11),
+      legend.title = element_text(size = 14),
+      legend.background = element_rect(fill = "transparent", colour = NA),
+      legend.box.background = element_rect(fill = "transparent", colour = NA)
+    )
+  
+  # Assign a name for gridExtra
+  assign(paste0("p",i), p)
+  
+}
+
+grid.arrange(p1, p2, p3, p4, ncol=2, nrow=2,
+             top = textGrob("Group-Level Posterior Between-Subject SD Estimates and 95% HPD Intervals",
+                            gp = gpar(fontface = "bold", fontsize = 16)),
+             bottom = textGrob("Parameter",
+                               gp = gpar(fontface = "bold", fontsize = 14)),
+             left = textGrob("Group-Level Posterior Between-Subject SD",
+                             gp = gpar(fontface = "bold", fontsize = 14), rot = 90))
+
+
+###################################################################
+
+
 ################ Generate predicted signal (MCMC) #################
 rm(list = setdiff(ls(), c("MCMC_results","SPM_results")))
 
