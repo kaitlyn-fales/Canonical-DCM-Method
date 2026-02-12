fdcbaa8305a4dab162b4baf8fcf58164c203066a
kaitlyn-fales <krf5429@psu.edu>
2026-02-12 11:52:47 -0500

    simulation scripts for MCMCpar_SPMdgm

---
+++ HCP_Social_Task/SPM/Sensitivity_Sim/MCMCpar_SPMdgm/single-sub_phaseLR_maskL.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=1 
+#SBATCH --mem-per-cpu=4gb
+#SBATCH --time=48:00:00
+#SBATCH --account=statsresearch_sc_default
+#SBATCH --output=Output/output_phaseLR_maskL_%A_%a.out
+#SBATCH --array=1-100
+
+# Get started
+echo " "
+echo "Job started on `hostname` at `date`"
+echo " "
+
+# Environment setup
+module purge
+module use /gpfs/group/RISE/sw7/modules
+module load r/4.2.1
+
+export R_LIBS="~/R_packages"
+
+# Path to file list
+FILE_LIST="/storage/work/krf5429/Canonical-DCM-Method/HCP_Social_Task/SPM/Sensitivity_Sim/MCMCpar_SPMdgm/file_list_phaseLR_maskL.txt"
+
+# Pick the file corresponding to this array task
+FILE=$(sed -n "${SLURM_ARRAY_TASK_ID}p" $FILE_LIST)
+
+echo "Processing $FILE"
+
+Rscript single-sub_phaseLR_maskL.R "$FILE"
+
+# Finish up
+echo " "
+echo "Job Ended at `date`"
+echo " "
+
+
+
+
+
+
