1c57b0e564450db41d66ea1df056d47c061ccdc0
kaitlyn-fales <krf5429@psu.edu>
2026-04-20 19:37:21 -0400

    incorporate cdcm package functionality

--- Balloon_Simulation/canonical_mdl_diagA.R
+++ Balloon_Simulation/canonical_mdl_diagA.R
@@ -13,6 +13,7 @@ suppressPackageStartupMessages(library(tidyverse))
 suppressPackageStartupMessages(library(mcmcse))
 suppressPackageStartupMessages(library(momentLS))
 suppressPackageStartupMessages(library(R.matlab))
+suppressPackageStartupMessages(library(cdcm))
 
 # Load in simulated data from Matlab and convert to R compatible format
 matlab_dat <- readMat("Data/canonical_sim_data_diagA.mat")
@@ -30,33 +31,23 @@ idxs = list(A_idxs = matrix(c(2,1,
             B_idxs = matrix(c(2,1,2), byrow=T, ncol = 3),
             C_idxs = matrix(c(1,1), byrow=T, ncol=2))
 
-# Source functions from parent directory
-source("../canonical_dcm_functions.R")
-
 # Put data into form for sampler
 stan_dat <- get_stan_dat(dat, idxs)
 
-# Convergence check specs - 90% intervals with 5% tolerance
-num_param <- get_num_param(stan_dat)
-ess_check <- as.numeric(minESS(num_param, alpha = 0.1, eps = 0.05))
+# Convergence criterion - Multivariate ESS (95% HPD, 5% tolerance)
+minESS_check <- minESS_criterion(stan_dat, alpha = 0.05, eps = 0.05)
 
-# Compile stan program from parent directory
-canonical_dcm = cmdstanr::cmdstan_model("../canonical_dcm.stan")
+# Compile stan program
+canonical_dcm <- compile_cdcm()
 
 # Use pathfinder to get good initial values
 inits_list <- get_initial_vals(canonical_dcm, stan_dat)
 
 # Run sampler until convergence
-dcm_sample(mod = canonical_dcm, 
-           data = stan_dat, 
-           inits_list = inits_list, 
-           output_dir = output_dir,
-           basename = basename,
-           metric = "dense_e",
-           refresh = 100,
-           warmup_iter = 5000,
-           n_iter_chunk = 1000,
-           max_iter = 100000,
-           adapt_delta = 0.9,
-           seed = index,
-           chains = 1)                     
+results <- dcm_sample(mod = canonical_dcm, 
+                      data = stan_dat, 
+                      inits_list = inits_list, 
+                      output_dir = output_dir,
+                      basename = basename,
+                      ess_check = ess_check,
+                      seed = index)                     
