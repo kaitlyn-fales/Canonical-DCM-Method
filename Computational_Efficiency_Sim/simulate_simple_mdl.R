df7c1c00fe4a6533d19f44373e4f1ed5f3096438
kaitlyn-fales <krf5429@psu.edu>
2026-04-27 12:18:35 -0400

    update simulation replicates

--- Computational_Efficiency_Sim/simulate_simple_mdl.R
+++ Computational_Efficiency_Sim/simulate_simple_mdl.R
@@ -1,168 +1,155 @@
-# Simulating from the complex model with the diag(A) fixed to -0.5
-remove(list=ls())
+remove(list = ls())
 
-# Packages
-library(expm)
-library(posterior)
 library(cdcm)
 
-# Generating inputs
-##########################
-m = 2; n_u = 2; max_time = 150*2
-nu = list(nu_A = c(0.4,0.3), nu_B = c(-0.2), nu_C=c(0.3))
-idxs = list(A_idxs = matrix(c(2,1,
-                              1,2), byrow = T, ncol=2),
-            B_idxs = matrix(c(2,1,2), byrow=T, ncol = 3),
-            C_idxs = matrix(c(1,1), byrow=T, ncol=2))
-input = NULL
+# Simulation settings
+n_reps <- 50  
 
-gen_input = function(times, start_time, end_time, duration,between_duration){
-  input = rep(0,length(times))
+nscan <- 150
+TR <- 2
+m <- 2
+n_u <- 2
+max_time <- nscan * TR
+SNR <- 1.679376
+z0 <- rep(0.1, m)
+
+out_dir <- "Computational_Efficiency_Sim/Data"
+dir.create(out_dir, recursive = TRUE, showWarnings = FALSE)
+
+# Input generation
+gen_input <- function(times, start_time, end_time, duration, between_duration) {
+  input <- rep(0, length(times))
   
-  current_start_time = start_time
-  time_points = matrix(nrow = 0,ncol=2)
-  while(1){
-    current_end_time = current_start_time + duration 
-    if(current_end_time > end_time){current_end_time = end_time}
-    time_points = rbind(time_points, c(current_start_time, current_end_time))
-    if(current_end_time + between_duration < end_time){
-      current_start_time = current_end_time + between_duration 
-    }else{
+  current_start_time <- start_time
+  time_points <- matrix(nrow = 0, ncol = 2)
+  
+  while (TRUE) {
+    current_end_time <- current_start_time + duration
+    
+    if (current_end_time > end_time) {
+      current_end_time <- end_time
+    }
+    
+    time_points <- rbind(time_points, c(current_start_time, current_end_time))
+    
+    if (current_end_time + between_duration < end_time) {
+      current_start_time <- current_end_time + between_duration
+    } else {
       break
     }
   }
-  for(i in 1:nrow(time_points)){
-    input[time_points[i,1]<times & times <=time_points[i,2]] = 1
-  }
-  input
-}
-
-times <- seq(0, max_time, 2)
-
-if(is.null(input)){
-  input1  = gen_input(times,start_time = 20,end_time = max(times),duration = 20,between_duration=40)
-  input2  = gen_input(times,start_time = 40,end_time = max(times),duration = 20,between_duration=40)
-  input = cbind(input1, input2)
-}else{
-  if(length(times)!=nrow(input)){stop("length(input)!=nrow(input)")}
-}
-
-
-# Add some white Gaussian noise according to approximate SNR obtained from SPM motion data
-SNR = 1.679376
-
-# Simulate data using cdcm
-sim <- simulate_cdcm(
-  nu = nu,
-  hypothesis_idxs = idxs,
-  nscan = 150,
-  m = m,
-  n_u = n_u,
-  TR = 2,
-  U = input,
-  SNR = SNR,
-  seed = c(1:m),
-  z0 = rep(0.1,m)
-)
-
-dat <- sim$simulated_data
-
-par(mfrow=c(1,2))
-for (i in 1:m){
-  plot(dat$times,dat$y_obs[,i],col=1,
-       type='l',lty=2,xlab = "Time",ylab = "Signal")
-}
-mtext(paste("Simulated Noisy Observations at SNR = ",
-            SNR,sep = ""), side = 3, line = -2, outer = T)
-
-##########################################
-
-# Save/export data
-save(dat, file = "Computational_Efficiency_Sim/Data/dat_simple_mdl_diag_fix.RData")
-
-############################
-
-
-################################################################################
-# Simulating from the complex model with the diag(A) allowed to vary
-remove(list=ls())
-
-# Generating inputs
-##########################
-m = 2; n_u = 2; max_time = 150*2
-nu = list(nu_A = c(0.4,0.3,rep(0.1,m)), nu_B = c(-0.2), nu_C=c(0.3))
-idxs = list(A_idxs = matrix(c(2,1,
-                              1,2,
-                              1,1,
-                              2,2), byrow = T, ncol=2),
-            B_idxs = matrix(c(2,1,2), byrow=T, ncol = 3),
-            C_idxs = matrix(c(1,1), byrow=T, ncol=2))
-input = NULL
-
-gen_input = function(times, start_time, end_time, duration,between_duration){
-  input = rep(0,length(times))
   
-  current_start_time = start_time
-  time_points = matrix(nrow = 0,ncol=2)
-  while(1){
-    current_end_time = current_start_time + duration 
-    if(current_end_time > end_time){current_end_time = end_time}
-    time_points = rbind(time_points, c(current_start_time, current_end_time))
-    if(current_end_time + between_duration < end_time){
-      current_start_time = current_end_time + between_duration 
-    }else{
-      break
-    }
+  for (i in 1:nrow(time_points)) {
+    input[time_points[i, 1] < times & times <= time_points[i, 2]] <- 1
   }
-  for(i in 1:nrow(time_points)){
-    input[time_points[i,1]<times & times <=time_points[i,2]] = 1
-  }
+  
   input
 }
 
-times <- seq(0, max_time, 2)
+times <- seq(0, max_time, TR)
 
-if(is.null(input)){
-  input1  = gen_input(times,start_time = 20,end_time = max(times),duration = 20,between_duration=40)
-  input2  = gen_input(times,start_time = 40,end_time = max(times),duration = 20,between_duration=40)
-  input = cbind(input1, input2)
-}else{
-  if(length(times)!=nrow(input)){stop("length(input)!=nrow(input)")}
-}
-
-
-# Add some white Gaussian noise according to approximate SNR obtained from SPM motion data
-SNR = 1.679376
-
-# Simulate data using cdcm
-sim <- simulate_cdcm(
-  nu = nu,
-  hypothesis_idxs = idxs,
-  nscan = 150,
-  m = m,
-  n_u = n_u,
-  TR = 2,
-  U = input,
-  SNR = SNR,
-  seed = c(1:m),
-  z0 = rep(0.1,m)
+input1 <- gen_input(
+  times = times,
+  start_time = 20,
+  end_time = max(times),
+  duration = 20,
+  between_duration = 40
 )
 
-dat <- sim$simulated_data
+input2 <- gen_input(
+  times = times,
+  start_time = 40,
+  end_time = max(times),
+  duration = 20,
+  between_duration = 40
+)
 
-par(mfrow=c(1,2))
-for (i in 1:m){
-  plot(dat$times,dat$y_obs[,i],col=1,
-       type='l',lty=2,xlab = "Time",ylab = "Signal")
+input <- cbind(input1, input2)
+
+# Model specifications
+models <- list(
+  diag_fix = list(
+    nu = list(
+      nu_A = c(0.4, 0.3),
+      nu_B = c(-0.2),
+      nu_C = c(0.3)
+    ),
+    idxs = list(
+      A_idxs = matrix(c(
+        2, 1,
+        1, 2
+      ), byrow = TRUE, ncol = 2),
+      B_idxs = matrix(c(2, 1, 2), byrow = TRUE, ncol = 3),
+      C_idxs = matrix(c(1, 1), byrow = TRUE, ncol = 2)
+    )
+  ),
+  
+  diag_var = list(
+    nu = list(
+      nu_A = c(0.4, 0.3, rep(0.1, m)),
+      nu_B = c(-0.2),
+      nu_C = c(0.3)
+    ),
+    idxs = list(
+      A_idxs = matrix(c(
+        2, 1,
+        1, 2,
+        1, 1,
+        2, 2
+      ), byrow = TRUE, ncol = 2),
+      B_idxs = matrix(c(2, 1, 2), byrow = TRUE, ncol = 3),
+      C_idxs = matrix(c(1, 1), byrow = TRUE, ncol = 2)
+    )
+  )
+)
+
+
+# Simulation function
+simulate_one_rep <- function(model_spec, rep_id, sim_seed_base) {
+  
+  # One seed per region for additive noise
+  # Example for rep 1 and m = 2: 1001, 1002
+  sim_seed <- sim_seed_base + seq_len(m)
+  
+  sim <- simulate_cdcm(
+    nu = model_spec$nu,
+    hypothesis_idxs = model_spec$idxs,
+    nscan = nscan,
+    m = m,
+    n_u = n_u,
+    TR = TR,
+    U = input,
+    SNR = SNR,
+    seed = sim_seed,
+    z0 = z0
+  )
+  
+  dat <- sim$simulated_data
+  
+  return(dat)
 }
-mtext(paste("Simulated Noisy Observations at SNR = ",
-            SNR,sep = ""), side = 3, line = -2, outer = T)
-##########################################
 
-# Save/export data
-save(dat, file = "Computational_Efficiency_Sim/Data/dat_simple_mdl_diag_var.RData")
+# Generate and save replicates
+set.seed(12345)
 
-############################
+# One base seed per simulation realization
+sim_seed_bases <- sample.int(1e7, size = n_reps)
+
+for (model_name in names(models)) {
+  
+  for (rep_id in seq_len(n_reps)) {
+    
+    dat <- simulate_one_rep(
+      model_spec = models[[model_name]],
+      rep_id = rep_id,
+      sim_seed_base = sim_seed_bases[rep_id]
+    )
+    
+    save(dat, file = file.path(out_dir,sprintf("dat_simple_mdl_%s_rep%03d.RData", model_name, rep_id)
+      )
+    )
+  }
+}
 
 
 
